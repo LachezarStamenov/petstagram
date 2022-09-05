c6a7305b9e4b8fef1cedb7ceb10badb61107739d
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-09-05 16:17:33 +0300

    views home, dashboard, profile and photo details added.

--- main/urls.py
+++ main/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = ()
+from django.urls import path
+
+from main.views import show_home, show_profile, show_dashboard, show_pet_photo_details
+
+urlpatterns = (
+    path('', show_home, name='index'),
+    path('dashboard/', show_dashboard, name='dashboard'),
+    path('profile/', show_profile, name='profile'),
+    path('photo/details/<int:pk>/', show_pet_photo_details, name='pet_photo_details'),
+)
