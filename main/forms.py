e0f8349308225c685c0ff525ac9120ddf4d1d634
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-09-11 21:11:29 +0300

    BootstrapFormMixin added. Create form finalized.

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,31 @@
 from django import forms
 
+from main.helpers import BootstrapFormMixin
 from main.models import Profile
 
 
-class ProfileForm(forms.ModelForm):
+class ProfileForm(BootstrapFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'picture')
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter first name',
+        }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter last name',
+        }
+            ),
+            'picture': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter URL',
+                }
+            ),
+        }
--- main/helpers.py
+++ main/helpers.py
@@ -6,3 +6,13 @@ def get_profile():
     if profiles:
         return profiles[0]
     return None
+
+
+class BootstrapFormMixin:
+    fields = {}
+
+    def _init_bootstrap_form_controls(self):
+        for _, field in self.fields.items():
+            if 'class' not in field.widget.attrs:
+                field.widget.attrs['class'] = ''
+            field.widget.attrs['class'] += 'form-control'
